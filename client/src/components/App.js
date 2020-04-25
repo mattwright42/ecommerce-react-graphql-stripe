e82de402b50d65a35158749f8d9e5798d7faee63
Matthew Wright <matthewbwright@gmail.com>
2020-04-25 15:47:43 -0700

    connects React with GraphQL, fetches data from client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import { Container, Box, Heading } from 'gestalt';
 import './App.css';
 
+import Strapi from 'strapi-sdk-javascript/build/main';
+const apiurl = process.env.API_URL || 'http://localhost:1337';
+const strapi = new Strapi(apiurl);
+
 class App extends Component {
+  async componentDidMount() {
+    const response = await strapi.request('POST', '/graphql', {
+      data: {
+        query: `query {
+          brands {
+          _id
+          Name
+          Description
+          Image {
+            name
+          }
+        }
+      }`,
+      },
+    });
+    console.log(response);
+  }
   render() {
-    return <div>App</div>;
+    return (
+      <Container>
+        {/*Brands Sction*/}
+        <Box display="flex" justifyContent="center" marginBottom={2}>
+          {/* Brands Header */}
+          <Heading color="midnight" size="md">
+            Brew Brands
+          </Heading>
+        </Box>
+      </Container>
+    );
   }
 }
 
